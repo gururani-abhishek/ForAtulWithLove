92075f21e8bfe1784edb93b980dc379d84218495
gururani-abhishek <gururani.abhishek@gmail.com>
2023-09-02 04:08:39 +0530

    add java

---
+++ src/Application.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+class BoxWithNuts {
+    String color;
+    boolean nuts;
+
+    public void shakeShake() {
+        // will do something
+    }
+
+    // constructor style,
+    public BoxWithNuts(int x) {
+        System.out.println("constructor being called");
+    }
+}
+
+class TwoIntegers {
+    int x;
+    int y;
+}
+
+// q for atul => can a class be inside another class? if yes how.
+public class Application {
+
+    public static void main(String[] args) {
+//        System.out.println("hey jude");
+//
+//        // reference variable
+//        BoxWithNuts variable1 = new BoxWithNuts(5);
+//        variable1.color = "Red";
+//
+//        // dog cherry = new dog();
+//        // human jayesh = new dog();
+//
+//
+////        UKCoolBoy abhishek = pointsAtMe;
+//        // int x = 4;
+//
+//        // process data, or you store data....
+//        // primitive ways to store data -> primitive data types.
+////        int x = 1;
+//        char y = 'x';
+//        System.out.println((int)'A');
+//        // ascii chart 'A' -> 65
+//
+//
+//        // user defined data-type
+//        // in java we create user-defined data-type using class
+//
+//        boolean husband = true;
+//
+//        if(husband) {
+//            boolean wife = true;
+//        }
+//
+//        System.out.println(wife);
+
+
+
+
+//        int x = 0; // because it is a good habit
+//        Scanner sc = new Scanner(System.in);
+//        int y = sc.nextInt();
+//
+//        if(y % 2 == 0) {
+//            // if y is even
+//            x = x + 2;
+//        } else {
+//            x  = x + 3;
+//        }
+
+        // in a file there cannot be more than one variable with the same name. 
+        System.out.println(x);
+
+        // create a variable x
+        // create a variable y, take input from the user for variable y
+        // if y is odd, add 3 to x
+        // if y is even, add 2 to x
+
+
+        // flow of program was from 1..2..3..4..5..6
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Box.java
@@ -0,0 +1,6 @@
+public class Box {
+    String color = "black";
+    boolean nuts = false;
+    boolean chole = false;
+    boolean chane = true;
+}
